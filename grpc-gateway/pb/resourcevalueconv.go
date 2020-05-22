foxxnuaa/cloud

integrate ResourceDirectory to grpc-gw

grpc-gateway/pb/resourcevalueconv.go
@@ -1,0 +1,50 @@
+package pb
+
+import (
+	"github.com/go-ocf/cloud/resource-aggregate/cqrs"
+	pbRA "github.com/go-ocf/cloud/resource-aggregate/pb"
+	"github.com/go-ocf/go-coap"
+)
+
+func RAStatus2Status(s pbRA.Status) Status {
+	switch s {
+	case pbRA.Status_OK:
+		return Status_OK
+	case pbRA.Status_BAD_REQUEST:
+		return Status_BAD_REQUEST
+	case pbRA.Status_UNAUTHORIZED:
+		return Status_UNAUTHORIZED
+	case pbRA.Status_FORBIDDEN:
+		return Status_FORBIDDEN
+	case pbRA.Status_NOT_FOUND:
+		return Status_NOT_FOUND
+	case pbRA.Status_UNAVAILABLE:
+		return Status_UNAVAILABLE
+	case pbRA.Status_NOT_IMPLEMENTED:
+		return Status_NOT_IMPLEMENTED
+	case pbRA.Status_ACCEPTED:
+		return Status_ACCEPTED
+	case pbRA.Status_ERROR:
+		return Status_ERROR
+	}
+	return Status_UNKNOWN
+}
+
+func (r *ResourceId) ID() string {
+	return cqrs.MakeResourceId(r.GetDeviceId(), r.GetResourceLinkHref())
+}
+
+func RAContent2Content(s *pbRA.Content) *Content {
+	if s == nil {
+		return nil
+	}
+	contentType := s.GetContentType()
+	if contentType == "" {
+		contentType = coap.MediaType(s.GetCoapContentFormat()).String()
+	}
+
+	return &Content{
+		Data:        s.GetData(),
+		ContentType: contentType,
+	}
+}

grpc-gateway/service/deviceDirectory.go
@@ -1,0 +1,195 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/go-ocf/sdk/schema"
+
+	"github.com/go-ocf/cloud/grpc-gateway/pb"
+
+	"github.com/go-ocf/sdk/schema/cloud"
+
+	coap "github.com/go-ocf/go-coap"
+	"github.com/go-ocf/kit/codec/cbor"
+	"github.com/go-ocf/kit/codec/json"
+	"github.com/go-ocf/kit/strings"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/go-ocf/cloud/resource-aggregate/cqrs"
+	pbRA "github.com/go-ocf/cloud/resource-aggregate/pb"
+)
+
+// hasMatchingStatus returns true for matching a device state.
+// An empty statusFilter matches all device states.
+func hasMatchingStatus(isOnline bool, statusFilter []pb.GetDevicesRequest_Status) bool {
+	if len(statusFilter) == 0 {
+		return true
+	}
+	for _, f := range statusFilter {
+		switch f {
+		case pb.GetDevicesRequest_ONLINE:
+			if isOnline {
+				return true
+			}
+		case pb.GetDevicesRequest_OFFLINE:
+			if !isOnline {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+type DeviceDirectory struct {
+	projection    *Projection
+	userDeviceIds strings.Set
+}
+
+// NewDeviceDirectory creates new device directory.
+func NewDeviceDirectory(projection *Projection, deviceIds []string) *DeviceDirectory {
+	mapDeviceIds := make(strings.Set)
+	mapDeviceIds.Add(deviceIds...)
+
+	return &DeviceDirectory{projection: projection, userDeviceIds: mapDeviceIds}
+}
+
+func decodeContent(content *pbRA.Content, v interface{}) error {
+	if content == nil {
+		return fmt.Errorf("cannot parse empty content")
+	}
+
+	var decoder func([]byte, interface{}) error
+
+	switch content.ContentType {
+	case coap.AppCBOR.String(), coap.AppOcfCbor.String():
+		decoder = cbor.Decode
+	case coap.AppJSON.String():
+		decoder = json.Decode
+	default:
+		return fmt.Errorf("unsupported content type: %v", content.ContentType)
+	}
+
+	return decoder(content.Data, v)
+}
+
+type Device struct {
+	ID                string
+	Resource          *schema.Device
+	IsOnline          bool
+	cloudStateUpdated bool
+}
+
+func (d Device) ToProto() pb.Device {
+	var r pb.Device
+	if d.Resource != nil {
+		r = pb.SchemaDevice(*d.Resource).ToProto()
+	}
+	r.IsOnline = d.IsOnline
+	r.Id = d.ID
+	return r
+}
+
+func updateDevice(dev Device, resource *resourceCtx) (Device, error) {
+	cloudResourceTypes := make(strings.Set)
+	cloudResourceTypes.Add(cloud.StatusResourceTypes...)
+
+	switch {
+	case resource.resource.GetHref() == "/oic/d":
+		var devContent schema.Device
+		err := decodeContent(resource.content.GetContent(), &devContent)
+		if err != nil {
+			return dev, err
+		}
+		dev.Resource = &devContent
+		dev.ID = devContent.ID
+	case cloudResourceTypes.HasOneOf(resource.resource.GetResourceTypes()...):
+		var cloudStatus cloud.Status
+		err := decodeContent(resource.content.GetContent(), &cloudStatus)
+		if err != nil {
+			return dev, err
+		}
+		dev.IsOnline = cloudStatus.Online
+		dev.cloudStateUpdated = true
+	}
+	return dev, nil
+}
+
+func filterDevicesByStatus(resourceValues map[string]map[string]*resourceCtx, req *pb.GetDevicesRequest) ([]Device, error) {
+	devices := make([]Device, 0, len(resourceValues))
+	for deviceID, resources := range resourceValues {
+		var device Device
+		for _, resource := range resources {
+			dev, err := updateDevice(device, resource)
+			if err != nil {
+				return nil, fmt.Errorf("cannot process device resources: %w", err)
+			}
+			device = dev
+		}
+		if device.Resource == nil {
+			device.ID = deviceID
+		}
+		if !device.cloudStateUpdated {
+			continue
+		}
+		if hasMatchingStatus(device.IsOnline, req.StatusFilter) {
+			devices = append(devices, device)
+		}
+	}
+	return devices, nil
+}
+
+// filterDevices returns filtered device ids that match filter.
+// An empty deviceIDsFilter matches all device ids.
+func filterDevices(deviceIds strings.Set, deviceIDsFilter []string) strings.Set {
+	if len(deviceIDsFilter) == 0 {
+		return deviceIds
+	}
+	result := make(strings.Set)
+	for _, deviceID := range deviceIDsFilter {
+		if deviceIds.HasOneOf(deviceID) {
+			result.Add(deviceID)
+		}
+	}
+	return result
+}
+
+// GetDevices provides list state of devices.
+func (dd *DeviceDirectory) GetDevices(req *pb.GetDevicesRequest, srv pb.GrpcGateway_GetDevicesServer) (err error) {
+	deviceIds := filterDevices(dd.userDeviceIds, req.DeviceIdsFilter)
+	if len(deviceIds) == 0 {
+		return status.Errorf(codes.NotFound, "not found")
+	}
+
+	typeFilter := make(strings.Set)
+	typeFilter.Add(req.TypeFilter...)
+	resourceIdsFilter := make(strings.Set)
+	for deviceID := range deviceIds {
+		resourceIdsFilter.Add(cqrs.MakeResourceId(deviceID, "/oic/d"))
+		resourceIdsFilter.Add(cqrs.MakeResourceId(deviceID, cloud.StatusHref))
+	}
+
+	resourceValues, err := dd.projection.GetResourceCtxs(srv.Context(), resourceIdsFilter, typeFilter, deviceIds)
+	if err != nil {
+		return status.Errorf(codes.Internal, "cannot get resource links by device ids: %v", err)
+	}
+
+	devices, err := filterDevicesByStatus(resourceValues, req)
+	if err != nil {
+		return status.Errorf(codes.Internal, "cannot filter devices by status: %v", err)
+	}
+
+	if len(devices) == 0 {
+		return status.Errorf(codes.NotFound, "not found")
+	}
+
+	for _, device := range devices {
+		dev := device.ToProto()
+		err := srv.Send(&dev)
+		if err != nil {
+			return status.Errorf(codes.Canceled, "cannot send device: %v", err)
+		}
+	}
+
+	return nil
+}

grpc-gateway/service/retrieveResourcesValues_test.go
@@ -1,0 +1,115 @@
+package service_test
+
+import (
+	"context"
+	"crypto/tls"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/go-ocf/cloud/authorization/provider"
+	"github.com/go-ocf/cloud/grpc-gateway/pb"
+	grpcTest "github.com/go-ocf/cloud/grpc-gateway/test"
+	coap "github.com/go-ocf/go-coap"
+	kitNetGrpc "github.com/go-ocf/kit/net/grpc"
+	"github.com/go-ocf/sdk/schema/cloud"
+)
+
+func cmpResourceValues(t *testing.T, want []*pb.ResourceValue, got []*pb.ResourceValue) {
+	require.Len(t, got, len(want))
+	for idx := range want {
+		dataWant := want[idx].GetContent().GetData()
+		datagot := got[idx].GetContent().GetData()
+		want[idx].Content.Data = nil
+		got[idx].Content.Data = nil
+		require.Equal(t, want[idx], got[idx])
+		w := grpcTest.DecodeCbor(t, dataWant)
+		g := grpcTest.DecodeCbor(t, datagot)
+		require.Equal(t, w, g)
+	}
+}
+
+func TestRequestHandler_RetrieveResourcesValues(t *testing.T) {
+	deviceID := grpcTest.MustFindDeviceByName(grpcTest.TestDeviceName)
+	type args struct {
+		req *pb.RetrieveResourcesValuesRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		want    []*pb.ResourceValue
+	}{
+		{
+			name: "valid",
+			args: args{
+				req: &pb.RetrieveResourcesValuesRequest{
+					ResourceIdsFilter: []*pb.ResourceId{
+						{
+							DeviceId:         deviceID,
+							ResourceLinkHref: cloud.StatusHref,
+						},
+					},
+				},
+			},
+			want: []*pb.ResourceValue{
+				{
+					ResourceId: &pb.ResourceId{
+						DeviceId:         deviceID,
+						ResourceLinkHref: cloud.StatusHref,
+					},
+					Types: cloud.StatusResourceTypes,
+					Content: &pb.Content{
+						ContentType: coap.AppOcfCbor.String(),
+						Data: grpcTest.EncodeToCbor(t, map[string]interface{}{
+							"if":     cloud.StatusInterfaces,
+							"rt":     cloud.StatusResourceTypes,
+							"online": true,
+						}),
+					},
+					Status: pb.Status_OK,
+				},
+			},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), TEST_TIMEOUT)
+	defer cancel()
+	ctx = kitNetGrpc.CtxWithToken(ctx, provider.UserToken)
+
+	tearDown := grpcTest.SetUp(ctx, t)
+	defer tearDown()
+
+	conn, err := grpc.Dial(grpcTest.GRPC_HOST, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
+		RootCAs: grpcTest.GetRootCertificatePool(t),
+	})))
+	require.NoError(t, err)
+	c := pb.NewGrpcGatewayClient(conn)
+
+	shutdownDevSim := grpcTest.OnboardDevSim(ctx, t, c, deviceID, grpcTest.GW_HOST, grpcTest.GetAllBackendResourceLinks())
+	defer shutdownDevSim()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := c.RetrieveResourcesValues(ctx, tt.args.req)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				values := make([]*pb.ResourceValue, 0, 1)
+				for {
+					value, err := client.Recv()
+					if err == io.EOF {
+						break
+					}
+					require.NoError(t, err)
+					values = append(values, value)
+				}
+				cmpResourceValues(t, tt.want, values)
+			}
+		})
+	}
+}

plgd-dev/hub

grpc-gateway/pb/resourcevalueconv.go
@@ -1,0 +1,50 @@
+package pb
+
+import (
+	"github.com/go-ocf/cloud/resource-aggregate/cqrs"
+	pbRA "github.com/go-ocf/cloud/resource-aggregate/pb"
+	"github.com/go-ocf/go-coap/v2/message"
+)
+
+func RAStatus2Status(s pbRA.Status) Status {
+	switch s {
+	case pbRA.Status_OK:
+		return Status_OK
+	case pbRA.Status_BAD_REQUEST:
+		return Status_BAD_REQUEST
+	case pbRA.Status_UNAUTHORIZED:
+		return Status_UNAUTHORIZED
+	case pbRA.Status_FORBIDDEN:
+		return Status_FORBIDDEN
+	case pbRA.Status_NOT_FOUND:
+		return Status_NOT_FOUND
+	case pbRA.Status_UNAVAILABLE:
+		return Status_UNAVAILABLE
+	case pbRA.Status_NOT_IMPLEMENTED:
+		return Status_NOT_IMPLEMENTED
+	case pbRA.Status_ACCEPTED:
+		return Status_ACCEPTED
+	case pbRA.Status_ERROR:
+		return Status_ERROR
+	}
+	return Status_UNKNOWN
+}
+
+func (r *ResourceId) ID() string {
+	return cqrs.MakeResourceId(r.GetDeviceId(), r.GetResourceLinkHref())
+}
+
+func RAContent2Content(s *pbRA.Content) *Content {
+	if s == nil {
+		return nil
+	}
+	contentType := s.GetContentType()
+	if contentType == "" {
+		contentType = message.MediaType(s.GetCoapContentFormat()).String()
+	}
+
+	return &Content{
+		Data:        s.GetData(),
+		ContentType: contentType,
+	}
+}

grpc-gateway/service/deviceDirectory.go
@@ -1,0 +1,195 @@
+package service
+
+import (
+	"fmt"
+
+	"github.com/go-ocf/go-coap/v2/message"
+	"github.com/go-ocf/sdk/schema"
+
+	"github.com/go-ocf/cloud/grpc-gateway/pb"
+
+	"github.com/go-ocf/sdk/schema/cloud"
+
+	"github.com/go-ocf/kit/codec/cbor"
+	"github.com/go-ocf/kit/codec/json"
+	"github.com/go-ocf/kit/strings"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"github.com/go-ocf/cloud/resource-aggregate/cqrs"
+	pbRA "github.com/go-ocf/cloud/resource-aggregate/pb"
+)
+
+// hasMatchingStatus returns true for matching a device state.
+// An empty statusFilter matches all device states.
+func hasMatchingStatus(isOnline bool, statusFilter []pb.GetDevicesRequest_Status) bool {
+	if len(statusFilter) == 0 {
+		return true
+	}
+	for _, f := range statusFilter {
+		switch f {
+		case pb.GetDevicesRequest_ONLINE:
+			if isOnline {
+				return true
+			}
+		case pb.GetDevicesRequest_OFFLINE:
+			if !isOnline {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+type DeviceDirectory struct {
+	projection    *Projection
+	userDeviceIds strings.Set
+}
+
+// NewDeviceDirectory creates new device directory.
+func NewDeviceDirectory(projection *Projection, deviceIds []string) *DeviceDirectory {
+	mapDeviceIds := make(strings.Set)
+	mapDeviceIds.Add(deviceIds...)
+
+	return &DeviceDirectory{projection: projection, userDeviceIds: mapDeviceIds}
+}
+
+func decodeContent(content *pbRA.Content, v interface{}) error {
+	if content == nil {
+		return fmt.Errorf("cannot parse empty content")
+	}
+
+	var decoder func([]byte, interface{}) error
+
+	switch content.ContentType {
+	case message.AppCBOR.String(), message.AppOcfCbor.String():
+		decoder = cbor.Decode
+	case message.AppJSON.String():
+		decoder = json.Decode
+	default:
+		return fmt.Errorf("unsupported content type: %v", content.ContentType)
+	}
+
+	return decoder(content.Data, v)
+}
+
+type Device struct {
+	ID                string
+	Resource          *schema.Device
+	IsOnline          bool
+	cloudStateUpdated bool
+}
+
+func (d Device) ToProto() pb.Device {
+	var r pb.Device
+	if d.Resource != nil {
+		r = pb.SchemaDevice(*d.Resource).ToProto()
+	}
+	r.IsOnline = d.IsOnline
+	r.Id = d.ID
+	return r
+}
+
+func updateDevice(dev Device, resource *resourceCtx) (Device, error) {
+	cloudResourceTypes := make(strings.Set)
+	cloudResourceTypes.Add(cloud.StatusResourceTypes...)
+
+	switch {
+	case resource.resource.GetHref() == "/oic/d":
+		var devContent schema.Device
+		err := decodeContent(resource.content.GetContent(), &devContent)
+		if err != nil {
+			return dev, err
+		}
+		dev.Resource = &devContent
+		dev.ID = devContent.ID
+	case cloudResourceTypes.HasOneOf(resource.resource.GetResourceTypes()...):
+		var cloudStatus cloud.Status
+		err := decodeContent(resource.content.GetContent(), &cloudStatus)
+		if err != nil {
+			return dev, err
+		}
+		dev.IsOnline = cloudStatus.Online
+		dev.cloudStateUpdated = true
+	}
+	return dev, nil
+}
+
+func filterDevicesByStatus(resourceValues map[string]map[string]*resourceCtx, req *pb.GetDevicesRequest) ([]Device, error) {
+	devices := make([]Device, 0, len(resourceValues))
+	for deviceID, resources := range resourceValues {
+		var device Device
+		for _, resource := range resources {
+			dev, err := updateDevice(device, resource)
+			if err != nil {
+				return nil, fmt.Errorf("cannot process device resources: %w", err)
+			}
+			device = dev
+		}
+		if device.Resource == nil {
+			device.ID = deviceID
+		}
+		if !device.cloudStateUpdated {
+			continue
+		}
+		if hasMatchingStatus(device.IsOnline, req.StatusFilter) {
+			devices = append(devices, device)
+		}
+	}
+	return devices, nil
+}
+
+// filterDevices returns filtered device ids that match filter.
+// An empty deviceIDsFilter matches all device ids.
+func filterDevices(deviceIds strings.Set, deviceIDsFilter []string) strings.Set {
+	if len(deviceIDsFilter) == 0 {
+		return deviceIds
+	}
+	result := make(strings.Set)
+	for _, deviceID := range deviceIDsFilter {
+		if deviceIds.HasOneOf(deviceID) {
+			result.Add(deviceID)
+		}
+	}
+	return result
+}
+
+// GetDevices provides list state of devices.
+func (dd *DeviceDirectory) GetDevices(req *pb.GetDevicesRequest, srv pb.GrpcGateway_GetDevicesServer) (err error) {
+	deviceIds := filterDevices(dd.userDeviceIds, req.DeviceIdsFilter)
+	if len(deviceIds) == 0 {
+		return status.Errorf(codes.NotFound, "not found")
+	}
+
+	typeFilter := make(strings.Set)
+	typeFilter.Add(req.TypeFilter...)
+	resourceIdsFilter := make(strings.Set)
+	for deviceID := range deviceIds {
+		resourceIdsFilter.Add(cqrs.MakeResourceId(deviceID, "/oic/d"))
+		resourceIdsFilter.Add(cqrs.MakeResourceId(deviceID, cloud.StatusHref))
+	}
+
+	resourceValues, err := dd.projection.GetResourceCtxs(srv.Context(), resourceIdsFilter, typeFilter, deviceIds)
+	if err != nil {
+		return status.Errorf(codes.Internal, "cannot get resource links by device ids: %v", err)
+	}
+
+	devices, err := filterDevicesByStatus(resourceValues, req)
+	if err != nil {
+		return status.Errorf(codes.Internal, "cannot filter devices by status: %v", err)
+	}
+
+	if len(devices) == 0 {
+		return status.Errorf(codes.NotFound, "not found")
+	}
+
+	for _, device := range devices {
+		dev := device.ToProto()
+		err := srv.Send(&dev)
+		if err != nil {
+			return status.Errorf(codes.Canceled, "cannot send device: %v", err)
+		}
+	}
+
+	return nil
+}

grpc-gateway/service/retrieveResourcesValues_test.go
@@ -1,0 +1,115 @@
+package service_test
+
+import (
+	"context"
+	"crypto/tls"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+
+	"github.com/go-ocf/cloud/authorization/provider"
+	"github.com/go-ocf/cloud/grpc-gateway/pb"
+	grpcTest "github.com/go-ocf/cloud/grpc-gateway/test"
+	"github.com/go-ocf/go-coap/v2/message"
+	kitNetGrpc "github.com/go-ocf/kit/net/grpc"
+	"github.com/go-ocf/sdk/schema/cloud"
+)
+
+func cmpResourceValues(t *testing.T, want []*pb.ResourceValue, got []*pb.ResourceValue) {
+	require.Len(t, got, len(want))
+	for idx := range want {
+		dataWant := want[idx].GetContent().GetData()
+		datagot := got[idx].GetContent().GetData()
+		want[idx].Content.Data = nil
+		got[idx].Content.Data = nil
+		require.Equal(t, want[idx], got[idx])
+		w := grpcTest.DecodeCbor(t, dataWant)
+		g := grpcTest.DecodeCbor(t, datagot)
+		require.Equal(t, w, g)
+	}
+}
+
+func TestRequestHandler_RetrieveResourcesValues(t *testing.T) {
+	deviceID := grpcTest.MustFindDeviceByName(grpcTest.TestDeviceName)
+	type args struct {
+		req *pb.RetrieveResourcesValuesRequest
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+		want    []*pb.ResourceValue
+	}{
+		{
+			name: "valid",
+			args: args{
+				req: &pb.RetrieveResourcesValuesRequest{
+					ResourceIdsFilter: []*pb.ResourceId{
+						{
+							DeviceId:         deviceID,
+							ResourceLinkHref: cloud.StatusHref,
+						},
+					},
+				},
+			},
+			want: []*pb.ResourceValue{
+				{
+					ResourceId: &pb.ResourceId{
+						DeviceId:         deviceID,
+						ResourceLinkHref: cloud.StatusHref,
+					},
+					Types: cloud.StatusResourceTypes,
+					Content: &pb.Content{
+						ContentType: message.AppOcfCbor.String(),
+						Data: grpcTest.EncodeToCbor(t, map[string]interface{}{
+							"if":     cloud.StatusInterfaces,
+							"rt":     cloud.StatusResourceTypes,
+							"online": true,
+						}),
+					},
+					Status: pb.Status_OK,
+				},
+			},
+		},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), TEST_TIMEOUT)
+	defer cancel()
+	ctx = kitNetGrpc.CtxWithToken(ctx, provider.UserToken)
+
+	tearDown := grpcTest.SetUp(ctx, t)
+	defer tearDown()
+
+	conn, err := grpc.Dial(grpcTest.GRPC_HOST, grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
+		RootCAs: grpcTest.GetRootCertificatePool(t),
+	})))
+	require.NoError(t, err)
+	c := pb.NewGrpcGatewayClient(conn)
+
+	shutdownDevSim := grpcTest.OnboardDevSim(ctx, t, c, deviceID, grpcTest.GW_HOST, grpcTest.GetAllBackendResourceLinks())
+	defer shutdownDevSim()
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			client, err := c.RetrieveResourcesValues(ctx, tt.args.req)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				values := make([]*pb.ResourceValue, 0, 1)
+				for {
+					value, err := client.Recv()
+					if err == io.EOF {
+						break
+					}
+					require.NoError(t, err)
+					values = append(values, value)
+				}
+				cmpResourceValues(t, tt.want, values)
+			}
+		})
+	}
+}