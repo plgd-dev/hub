7b1ff73548b556d6efa44fee3c308f289fa5d796
Jozef Kralik <jozef.kralik@kistler.com>
2020-05-27 11:53:47 +0200

    remove race

--- grpc-gateway/service/subscriptions.go
+++ grpc-gateway/service/subscriptions.go
@@ -188,10 +188,6 @@ func (s *subscriptions) Pop(id string) Subscriber {
 				delete(s.devicesSubscriptions, userID)
 			}
 		}
-		delete(s.initSubscriptions[userID], id)
-		if len(s.initSubscriptions[userID]) == 0 {
-			delete(s.initSubscriptions, userID)
-		}
 		delete(s.allSubscriptions, id)
 		return sub
 	}
