d57cbbb3dcf1127468f710f8d540966e512a676b
Jozef Kralik <jozef.kralik@kistler.com>
2020-10-13 07:58:29 +0200

    generate certificates to files

--- bundle/client/get-manufacturer-certificate.sh
+++ bundle/client/get-manufacturer-certificate.sh
@@ -5,6 +5,8 @@ usage()
     echo "usage: get-manufacturer-certificate.sh [[[-c csr] [-t token] [-a address]] | [-h]]"
 }
 
+tmp_dir=$(mktemp -d -t mfgcert-XXXXXXXXXX)
+
 while [ "$1" != "" ]; do
     case $1 in
         -c | --csr )            shift
@@ -27,9 +29,9 @@ while [ "$1" != "" ]; do
 done
 
 if [ -z ${csr+x} ]; then
-    openssl ecparam -name secp256r1 -genkey -noout -out /tmp/mfg.key.pem
-    cat /tmp/mfg.key.pem
-    csr=`openssl req -new -sha256 -key /tmp/mfg.key.pem -subj "/CN=mfgCert" -addext extendedKeyUsage=serverAuth,clientAuth -addext keyUsage=digitalSignature,keyAgreement`
+    openssl ecparam -name prime256v1 -genkey -noout -out ${tmp_dir}/mfgcert.key.pem
+    
+    csr=`openssl req -new -sha256 -key ${tmp_dir}/mfgcert.key.pem -subj "/CN=mfgCert" -addext extendedKeyUsage=serverAuth,clientAuth -addext keyUsage=digitalSignature,keyAgreement`
 fi
 
 if [ -z ${token+x} ]; then
@@ -40,4 +42,12 @@ if [ -z ${host+x} ]; then
     host="-k https://localhost:9086"
 fi
 
-curl -s -d "{\"CSR\":\"${csr}\"}" -H 'Content-Type: application/json' -H "Authorization: Bearer ${token}" ${host}/api/v1/certificate-authority/sign | jq -r .CertificateChain
+curl -s -d "{\"CSR\":\"${csr}\"}" -H 'Content-Type: application/json' -H "Authorization: Bearer ${token}" ${host}/api/v1/certificate-authority/sign | jq -r .CertificateChain > ${tmp_dir}/mfgcert.pem
+cat ${tmp_dir}/mfgcert.pem | tac | awk '/END/,/BEGIN/{ if(/BEGIN/){print; exit}; print}' | tac > ${tmp_dir}/mfgtrustca.pem
+echo "{"
+if [ -f ${tmp_dir}/mfgcert.key.pem ]; then
+    echo "  \"mfgcert-key\": \"${tmp_dir}/mfgcert.key.pem\","
+fi
+echo "  \"mfgcert\": \"${tmp_dir}/mfgcert.pem\","
+echo "  \"mfgtrustca\": \"${tmp_dir}/mfgtrustca.pem\""
+echo "}"
