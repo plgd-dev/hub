3995496b410831e6f4ee833abf3c2dbdf7b536c5
Jozef Kralik <jozef.kralik@kistler.com>
2020-12-04 18:19:28 +0000

    fix test

--- resource-aggregate/refImpl/refImpl_test.go
+++ resource-aggregate/refImpl/refImpl_test.go
@@ -4,13 +4,25 @@ import (
 	"testing"
 
 	"github.com/kelseyhightower/envconfig"
+	authConfig "github.com/plgd-dev/cloud/authorization/service"
+	authService "github.com/plgd-dev/cloud/authorization/test"
 	"github.com/stretchr/testify/require"
 )
 
 func TestInit(t *testing.T) {
-	var config Config
-	err := envconfig.Process("", &config)
+	var authCfg authConfig.Config
+	err := envconfig.Process("", &authCfg)
 	require.NoError(t, err)
+	authCfg.Addr = "localhost:12345"
+	authCfg.HTTPAddr = "localhost:12346"
+	authCfg.Device.Provider = "test"
+	authShutdown := authService.New(t, authCfg)
+	defer authShutdown()
+
+	var config Config
+	err = envconfig.Process("", &config)
+	require.NoError(t, err)
+	config.Service.OAuth.Endpoint.TokenURL = "https://" + authCfg.HTTPAddr + "/api/authz/token"
 
 	got, err := Init(config)
 	require.NoError(t, err)
