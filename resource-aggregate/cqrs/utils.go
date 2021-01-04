758d1a72d1882c3e33b6a4eda903077bcf2851f9
Jozef Kralik <jozef.kralik@kistler.com>
2021-01-04 13:31:44 +0000

    fix marshal/unmarshal message

--- resource-aggregate/cqrs/utils.go
+++ resource-aggregate/cqrs/utils.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gofrs/uuid"
 	"github.com/golang/snappy"
 	"github.com/plgd-dev/cloud/resource-aggregate/pb"
-	"google.golang.org/protobuf/proto"
 )
 
 func GetTopics(deviceId string) []string {
@@ -51,8 +50,8 @@ type ProtobufUnmarshaler interface {
 }
 
 func Marshal(v interface{}) ([]byte, error) {
-	if p, ok := v.(proto.Message); ok {
-		src, err := proto.Marshal(p)
+	if p, ok := v.(ProtobufMarshaler); ok {
+		src, err := p.Marshal()
 		if err != nil {
 			return nil, fmt.Errorf("cannot marshal event: %w", err)
 		}
@@ -63,13 +62,13 @@ func Marshal(v interface{}) ([]byte, error) {
 }
 
 func Unmarshal(b []byte, v interface{}) error {
-	if p, ok := v.(proto.Message); ok {
+	if p, ok := v.(ProtobufUnmarshaler); ok {
 		dst := make([]byte, 1024)
 		dst, err := snappy.Decode(dst, b)
 		if err != nil {
 			return fmt.Errorf("cannot decode buffer: %w", err)
 		}
-		return proto.Unmarshal(dst, p)
+		return p.Unmarshal(dst)
 	}
 	return fmt.Errorf("marshal is not supported by %T", v)
 }
--- resource-aggregate/cqrs/utils_test.go
+++ resource-aggregate/cqrs/utils_test.go
@@ -1,50 +1,51 @@
-package cqrs
+package cqrs_test
 
 import (
 	"testing"
 
-	"github.com/plgd-dev/cloud/resource-aggregate/pb"
+	"github.com/plgd-dev/cloud/resource-aggregate/cqrs"
+	"github.com/plgd-dev/cloud/resource-aggregate/cqrs/events"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestDummyForCoverage(t *testing.T) {
 	deviceID := "dev"
 
-	GetTopics(deviceID)
-	MakeResourceId(deviceID, "/abc")
+	cqrs.GetTopics(deviceID)
+	cqrs.MakeResourceId(deviceID, "/abc")
 	sequence := uint64(1234)
 	version := uint64(5)
 	connId := "c"
 	corID := "a"
 	userID := "u"
 
-	TimeNowMs()
-	em := MakeEventMeta(connId, sequence, version)
+	cqrs.TimeNowMs()
+	em := cqrs.MakeEventMeta(connId, sequence, version)
 	assert.Equal(t, connId, em.ConnectionId)
 	assert.Equal(t, sequence, em.Sequence)
 	assert.Equal(t, version, em.Version)
-	ac := MakeAuditContext(deviceID, userID, corID)
+	ac := cqrs.MakeAuditContext(deviceID, userID, corID)
 	assert.Equal(t, corID, ac.CorrelationId)
 	assert.Equal(t, userID, ac.UserId)
 	assert.Equal(t, deviceID, ac.DeviceId)
 }
 
 func TestProtobufMarshaler(t *testing.T) {
-	req := pb.AuthorizationContext{}
+	req := events.ResourceChanged{}
 
-	out, err := Marshal(&req)
+	out, err := cqrs.Marshal(&req)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, out)
 
 	a := struct {
 	}{}
-	_, err = Marshal(a)
+	_, err = cqrs.Marshal(a)
 	assert.Error(t, err)
 
-	resp := pb.AuthorizationContext{}
-	err = Unmarshal(out, &resp)
+	resp := events.ResourceChanged{}
+	err = cqrs.Unmarshal(out, &resp)
 	assert.NoError(t, err)
 
-	err = Unmarshal(out, a)
+	err = cqrs.Unmarshal(out, a)
 	assert.Error(t, err)
 }
