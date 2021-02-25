3a5b6f2f18563c084b4b7d830ba96d9d7c97cf35
Jozef Kralik <jozef.kralik@kistler.com>
2021-02-25 08:23:29 +0000

    authorization: dont send codes.Unauthenticated when cannot establish connection to oauth server

--- authorization/service/signup.go
+++ authorization/service/signup.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"strings"
 
 	"github.com/plgd-dev/cloud/authorization/pb"
 	"github.com/plgd-dev/cloud/authorization/persistence"
@@ -25,7 +26,11 @@ func (s *Service) SignUp(ctx context.Context, request *pb.SignUpRequest) (*pb.Si
 
 	token, err := s.deviceProvider.Exchange(ctx, request.AuthorizationProvider, request.AuthorizationCode)
 	if err != nil {
-		return nil, logAndReturnError(status.Errorf(codes.Unauthenticated, "cannot sign up: %v", err.Error()))
+		code := codes.Unauthenticated
+		if strings.Contains(err.Error(), "connect: connection refused") {
+			code = codes.Unavailable
+		}
+		return nil, logAndReturnError(status.Errorf(code, "cannot sign up: %v", err.Error()))
 	}
 
 	dev, ok, err := tx.RetrieveByDevice(request.DeviceId)
