bcc77ecf26ff394c5427ce3d6460d723932f1028
Tomas Kubovic <kuby000@gmail.com>
2021-03-10 20:22:19 +0100

    Browser notifications

--- http-gateway/web/src/containers/things/_things-list-header.js
+++ http-gateway/web/src/containers/things/_things-list-header.js
@@ -48,9 +48,16 @@ export const ThingsListHeader = ({ loading, refresh }) => {
         id="status-notifications"
         label={_(t.notifications)}
         checked={enabled}
-        onChange={() =>
+        onChange={e => {
+          if (e.target.checked) {
+            // Request browser notifications
+            // (browsers will explicitly disallow notification permission requests not triggered in response to a user gesture,
+            // so we must call it to make sure the user has received a notification request)
+            Notification.requestPermission()
+          }
+
           dispatch(toggleActiveNotification(THINGS_STATUS_WS_KEY))
-        }
+        }}
       />
     </div>
   )
--- http-gateway/web/src/containers/things/things-details-page.js
+++ http-gateway/web/src/containers/things/things-details-page.js
@@ -3,6 +3,7 @@ import { useIntl } from 'react-intl'
 import { useParams } from 'react-router-dom'
 import classNames from 'classnames'
 
+import { history } from '@/store'
 import { ConfirmModal } from '@/components/confirm-modal'
 import { Layout } from '@/components/layout'
 import { NotFoundPage } from '@/containers/not-found-page'
@@ -48,11 +49,11 @@ export const ThingsDetailsPage = () => {
   // Open the resource modal when href is present
   useEffect(
     () => {
-      if (href) {
+      if (href && !loading) {
         openUpdateModal({ href: `/${href}` })
       }
     },
-    [href] // eslint-disable-line
+    [href, loading] // eslint-disable-line
   )
 
   if (error) {
@@ -257,6 +258,16 @@ export const ThingsDetailsPage = () => {
     }
   }
 
+  // Handler which cleans up the resource modal data and updates the URL
+  const handleCloseUpdateModal = () => {
+    setResourceModalData(null)
+
+    if (href) {
+      // Remove the href from the URL when the update modal is closed
+      history.replace(window.location.pathname.replace(`/${href}`, ''))
+    }
+  }
+
   return (
     <Layout
       title={`${deviceName ? deviceName + ' | ' : ''}${_(menuT.things)}`}
@@ -265,6 +276,7 @@ export const ThingsDetailsPage = () => {
       header={
         <ThingsDetailsHeader
           deviceId={data?.device?.di}
+          deviceName={deviceName}
           isUnregistered={isUnregistered}
         />
       }
@@ -293,7 +305,7 @@ export const ThingsDetailsPage = () => {
 
       <ThingsResourcesModal
         {...resourceModalData}
-        onClose={() => setResourceModalData(null)}
+        onClose={handleCloseUpdateModal}
         fetchResource={openUpdateModal}
         updateResource={updateResource}
         createResource={createResource}
@@ -302,6 +314,7 @@ export const ThingsDetailsPage = () => {
         isDeviceOnline={isOnline}
         isUnregistered={isUnregistered}
         deviceId={id}
+        deviceName={deviceName}
       />
 
       <ConfirmModal
