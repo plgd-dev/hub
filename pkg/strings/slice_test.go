bf5fd5f8015a1b523f9780136c6888ac9b772db8
Daniel Adam <daniel.adam1922@protonmail.com>
2021-08-12 20:28:43 +0200

    pkg: add Split and Unique function to strings package

--- pkg/strings/slice_test.go
+++ pkg/strings/slice_test.go
@@ -1,6 +1,8 @@
 package strings
 
 import (
+	"sort"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -63,6 +65,109 @@ func TestIntersection(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Intersection(tt.args.s1, tt.args.s2)
+			require.Equal(t, got, tt.want)
+		})
+	}
+}
+
+func isNumeric(s string) bool {
+	_, err := strconv.ParseInt(s, 10, 64)
+	return err == nil
+}
+
+func TestSplit(t *testing.T) {
+	type args struct {
+		s []string
+		f SplitFilter
+	}
+	tests := []struct {
+		name      string
+		args      args
+		wantTrue  []string
+		wantFalse []string
+	}{
+		{
+			name: "Empty",
+			args: args{
+				f: isNumeric,
+			},
+		},
+		{
+			name: "All true",
+			args: args{
+				s: []string{"1", "42", "1337"},
+				f: isNumeric,
+			},
+			wantTrue: []string{"1", "42", "1337"},
+		},
+		{
+			name: "All false",
+			args: args{
+				s: []string{"a", "bc", "leet"},
+				f: isNumeric,
+			},
+			wantFalse: []string{"a", "bc", "leet"},
+		},
+		{
+			name: "Some true",
+			args: args{
+				s: []string{"first", "42", "middle", "1337", "last"},
+				f: isNumeric,
+			},
+			wantTrue:  []string{"42", "1337"},
+			wantFalse: []string{"first", "middle", "last"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotTrue, gotFalse := Split(tt.args.s, tt.args.f)
+			require.Equal(t, gotTrue, tt.wantTrue)
+			require.Equal(t, gotFalse, tt.wantFalse)
+		})
+	}
+}
+
+func TestUnique(t *testing.T) {
+	type args struct {
+		s []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Empty",
+			args: args{},
+			want: nil,
+		},
+		{
+			name: "Single",
+			args: args{
+				s: []string{"a", "a", "a"},
+			},
+			want: []string{"a"},
+		},
+		{
+			name: "Single",
+			args: args{
+				s: []string{"a", "a", "a"},
+			},
+			want: []string{"a"},
+		},
+		{
+			name: "Multiple",
+			args: args{
+				s: []string{"a", "a", "a", "b", "b", "c"},
+			},
+			want: []string{"a", "b", "c"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := Unique(tt.args.s)
+			sort.Strings(got)
+			sort.Strings(tt.want)
 			require.Equal(t, got, tt.want)
 		})
 	}
