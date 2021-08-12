aadb8c7b710538f595828cdca7b5c356ad3b65c5
Daniel Adam <daniel.adam1922@protonmail.com>
2021-08-12 20:28:43 +0200

    pkg: add Split and Unique function to strings package

--- pkg/strings/slice.go
+++ pkg/strings/slice.go
@@ -19,3 +19,44 @@ func Intersection(s1, s2 []string) []string {
 	}
 	return inter
 }
+
+type SplitFilter func(s string) bool
+
+// Split one slice into to based on some filter.
+//
+// Function returns two slices, first slice contains all elements of the input slice
+// that satisfy the filter, the second slice contains the rest.
+func Split(s []string, f SplitFilter) ([]string, []string) {
+	var s1, s2 []string
+	for _, v := range s {
+		if f(v) {
+			s1 = append(s1, v)
+		} else {
+			s2 = append(s2, v)
+		}
+	}
+
+	return s1, s2
+}
+
+// Return slice of unique elements of the input slice.
+//
+// The function is not stable, the order of elements might change.
+func Unique(s []string) []string {
+	if l := len(s); l == 0 {
+		return nil
+	}
+
+	set := make(map[string]struct{})
+	for _, v := range s {
+		set[v] = struct{}{}
+	}
+
+	keys := make([]string, len(set))
+	i := 0
+	for k := range set {
+		keys[i] = k
+		i++
+	}
+	return keys
+}
