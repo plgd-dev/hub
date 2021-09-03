6fe8ee4fab61783bddb5b6ecf2c0cb889ac3c716
Tomas Kubovic <kuby000@gmail.com>
2021-09-03 18:33:13 +0200

    Removed leftover code

--- http-gateway/web/src/containers/pending-commands/_pending-command-details-modal.js
+++ http-gateway/web/src/containers/pending-commands/_pending-command-details-modal.js
@@ -1,12 +1,6 @@
-// import { useEffect, useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { useIntl } from 'react-intl'
 
-// import { Editor } from '@/components/editor'
-// import { Select } from '@/components/select'
-// import { Button } from '@/components/button'
-// import { Badge } from '@/components/badge'
-// import { Label } from '@/components/label'
 import { Modal } from '@/components/modal'
 
 import { commandTypes } from './constants'
