1de134f02afd04f07928475cd7dcb00e2ba233ea
Daniel Adam <daniel.adam1922@protonmail.com>
2021-10-05 18:56:23 +0200

    coap-gateway: avoid repeated Exchange calls
    Use exchangeCache in signUp handler to avoid sending
    multiple Exchange requests to oauth server with the same
    authorization code value.

--- coap-gateway/service/signUp_test.go
+++ coap-gateway/service/signUp_test.go
@@ -2,9 +2,12 @@ package service_test
 
 import (
 	"testing"
+	"time"
 
+	coapgwTest "github.com/plgd-dev/cloud/coap-gateway/test"
 	"github.com/plgd-dev/cloud/coap-gateway/uri"
 	"github.com/plgd-dev/cloud/test/config"
+	"github.com/plgd-dev/cloud/test/oauth-server/service"
 	oauthTest "github.com/plgd-dev/cloud/test/oauth-server/test"
 	coapCodes "github.com/plgd-dev/go-coap/v2/message/codes"
 )
@@ -40,5 +43,41 @@ func TestSignUpPostHandler(t *testing.T) {
 			testPostHandler(t, uri.SignUp, test, co)
 		}
 		t.Run(test.name, tf)
+	}
+}
+
+type TestCoapSignUpResponseRetry struct {
+	AccessToken  string `json:"-"`
+	RedirectURI  string `json:"-"`
+	ExpiresIn    uint64 `json:"-"`
+	RefreshToken string `json:"-"`
+	UserID       string `json:"uid"`
+}
+
+func TestSignUpPostHandlerWithRetry(t *testing.T) {
+	coapgwCfg := coapgwTest.MakeConfig(t)
+	coapgwCfg.APIs.COAP.Authorization.Providers[0].Config.ClientID = service.ClientTestRestrictedAuth
+	shutdown := setUp(t, coapgwCfg)
+	defer shutdown()
+	codeEl := oauthTest.GetDefaultDeviceAuthorizationCode(t, "")
+
+	co := testCoapDial(t, config.GW_HOST, "")
+	if co == nil {
+		return
+	}
+	defer func() {
+		_ = co.Close()
+	}()
+
+	testSignUp := testEl{
+		name: "Retry",
+		in:   input{coapCodes.POST, `{"di": "` + CertIdentity + `", "accesstoken":"` + codeEl + `", "authprovider": "` + config.DEVICE_PROVIDER + `"}`, nil},
+		out:  output{coapCodes.Changed, TestCoapSignUpResponseRetry{UserID: "1"}, nil},
+	}
+
+	const retryCount = 3
+	for i := 0; i < retryCount; i++ {
+		testPostHandler(t, uri.SignUp, testSignUp, co)
+		time.Sleep(time.Second)
 	}
 }
