b97c58150cb21fef025f930d726f4c3ac9053822
Daniel Adam <daniel.adam1922@protonmail.com>
2021-10-11 13:57:48 +0200

    cloud2cloud-connector: updates for SonarCloud (2)

--- cloud2cloud-connector/test/cloudOrigin.go
+++ cloud2cloud-connector/test/cloudOrigin.go
@@ -13,6 +13,7 @@ import (
 )
 
 const (
+	HTTPS_SCHEME            = "https://"
 	IDENTITY_STORE_HOST     = "localhost:30000"
 	RESOURCE_AGGREGATE_HOST = "localhost:30003"
 	RESOURCE_DIRECTORY_HOST = "localhost:30004"
@@ -24,9 +25,9 @@ const (
 )
 
 var (
-	C2C_CONNECTOR_EVENTS_URL        = "https://" + C2C_CONNECTOR_HOST + c2curi.Events
-	C2C_CONNECTOR_OAUTH_CALLBACK    = "https://" + C2C_CONNECTOR_HOST + "/oauthCbk"
-	OAUTH_MANAGER_ENDPOINT_TOKENURL = "https://" + OAUTH_HOST + uri.Token
+	C2C_CONNECTOR_EVENTS_URL        = HTTPS_SCHEME + C2C_CONNECTOR_HOST + c2curi.Events
+	C2C_CONNECTOR_OAUTH_CALLBACK    = HTTPS_SCHEME + C2C_CONNECTOR_HOST + "/oauthCbk"
+	OAUTH_MANAGER_ENDPOINT_TOKENURL = HTTPS_SCHEME + OAUTH_HOST + uri.Token
 )
 
 func SetUpCloudWithConnector(t *testing.T) (TearDown func()) {
@@ -37,14 +38,14 @@ func SetUpCloudWithConnector(t *testing.T) (TearDown func()) {
 
 	idCfg := idService.MakeConfig(t)
 	idCfg.APIs.GRPC.Addr = IDENTITY_STORE_HOST
-	idCfg.APIs.GRPC.Authorization.Authority = "https://" + OAUTH_HOST
+	idCfg.APIs.GRPC.Authorization.Authority = HTTPS_SCHEME + OAUTH_HOST
 	idCfg.Clients.Storage.MongoDB.Database = C2C_CONNECTOR_DB
 	idCfg.Clients.Eventbus.NATS.URL = C2C_CONNECTOR_NATS_URL
 	idShutdown := idService.New(t, idCfg)
 
 	raCfg := raService.MakeConfig(t)
 	raCfg.APIs.GRPC.Addr = RESOURCE_AGGREGATE_HOST
-	raCfg.APIs.GRPC.Authorization.Authority = "https://" + OAUTH_HOST
+	raCfg.APIs.GRPC.Authorization.Authority = HTTPS_SCHEME + OAUTH_HOST
 	raCfg.Clients.Eventstore.Connection.MongoDB.Embedded.Database = C2C_CONNECTOR_DB
 	raCfg.Clients.IdentityStore.Connection.Addr = IDENTITY_STORE_HOST
 	raCfg.Clients.Eventbus.NATS.URL = C2C_CONNECTOR_NATS_URL
@@ -52,7 +53,7 @@ func SetUpCloudWithConnector(t *testing.T) (TearDown func()) {
 
 	rdCfg := rdService.MakeConfig(t)
 	rdCfg.APIs.GRPC.Addr = RESOURCE_DIRECTORY_HOST
-	rdCfg.APIs.GRPC.Authorization.Authority = "https://" + OAUTH_HOST
+	rdCfg.APIs.GRPC.Authorization.Authority = HTTPS_SCHEME + OAUTH_HOST
 	rdCfg.Clients.Eventstore.Connection.MongoDB.Embedded.Database = C2C_CONNECTOR_DB
 	rdCfg.Clients.Eventbus.NATS.URL = C2C_CONNECTOR_NATS_URL
 	rdCfg.Clients.IdentityStore.Connection.Addr = IDENTITY_STORE_HOST
@@ -61,7 +62,7 @@ func SetUpCloudWithConnector(t *testing.T) (TearDown func()) {
 	grpcCfg := grpcService.MakeConfig(t)
 	grpcCfg.APIs.GRPC.Addr = GRPC_GATEWAY_HOST
 	grpcCfg.APIs.GRPC.TLS.ClientCertificateRequired = false
-	grpcCfg.APIs.GRPC.Authorization.Authority = "https://" + OAUTH_HOST
+	grpcCfg.APIs.GRPC.Authorization.Authority = HTTPS_SCHEME + OAUTH_HOST
 	grpcCfg.Clients.Eventbus.NATS.URL = C2C_CONNECTOR_NATS_URL
 	grpcCfg.Clients.ResourceAggregate.Connection.Addr = RESOURCE_AGGREGATE_HOST
 	grpcCfg.Clients.ResourceDirectory.Connection.Addr = RESOURCE_DIRECTORY_HOST
@@ -71,7 +72,7 @@ func SetUpCloudWithConnector(t *testing.T) (TearDown func()) {
 	c2cConnectorCfg.APIs.HTTP.EventsURL = C2C_CONNECTOR_EVENTS_URL
 	c2cConnectorCfg.APIs.HTTP.Connection.Addr = C2C_CONNECTOR_HOST
 	c2cConnectorCfg.APIs.HTTP.Authorization = MakeAuthorizationConfig()
-	c2cConnectorCfg.APIs.HTTP.Authorization.Authority = "https://" + OAUTH_HOST
+	c2cConnectorCfg.APIs.HTTP.Authorization.Authority = HTTPS_SCHEME + OAUTH_HOST
 	c2cConnectorCfg.APIs.HTTP.Authorization.Config.RedirectURL = C2C_CONNECTOR_OAUTH_CALLBACK
 	c2cConnectorCfg.Clients.Storage.MongoDB.Database = C2C_CONNECTOR_DB
 	c2cConnectorCfg.Clients.IdentityStore.Connection.Addr = IDENTITY_STORE_HOST
