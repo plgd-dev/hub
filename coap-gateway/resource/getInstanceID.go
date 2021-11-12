9d5e664e545f4680d3d19d53bead7eecebae7905
Daniel Adam <daniel.adam1922@protonmail.com>
2021-11-12 16:38:26 +0100

    coap-gw: move observation to stand-alone file

--- coap-gateway/resource/getInstanceID.go
+++ coap-gateway/resource/getInstanceID.go
@@ -1,8 +1,8 @@
-package service
+package resource
 
 import "hash/crc32"
 
-func getInstanceID(href string) int64 {
+func GetInstanceID(href string) int64 {
 	h := crc32.New(crc32.IEEETable)
 	h.Write([]byte(href))
 	return int64(h.Sum32())
