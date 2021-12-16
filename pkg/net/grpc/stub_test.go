0b13ee7a7b85086749aa41a791d758f40fba3ab0
Jozef Kralik <jojo.lwin@gmail.com>
2021-12-16 15:04:26 +0000

    replace deprecated function

--- pkg/net/grpc/stub_test.go
+++ pkg/net/grpc/stub_test.go
@@ -3,6 +3,7 @@ package grpc_test
 import (
 	"github.com/plgd-dev/hub/pkg/net/grpc/server"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func StubGrpcServer(opts ...grpc.ServerOption) *server.Server {
@@ -16,7 +17,7 @@ func StubGrpcServer(opts ...grpc.ServerOption) *server.Server {
 }
 
 func StubGrpcClient(addr string) StubServiceClient {
-	conn, err := grpc.Dial(addr, grpc.WithInsecure())
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		panic(err)
 	}
