a23f9049ecaf2383b3c221fc69968100469a75a0
PatrikMatiasko <patrik.matiasko@gmail.com>
2022-09-22 23:14:54 +0200

    fix sorting in table

--- http-gateway/web/src/components/table/utils.js
+++ http-gateway/web/src/components/table/utils.js
@@ -1,3 +1,7 @@
 // Case insensitive sort function
-export const compareIgnoreCase = (a, b) =>
-  a.localeCompare(b, 'en', { numeric: true, sensitivity: 'base' })
+export const compareIgnoreCase = (a, b) => {
+  const item1 = Array.isArray(a) ? a[0] : a
+  const item2 = Array.isArray(b) ? b[0] : b
+
+  return item1.localeCompare(item2, 'en', { numeric: true, sensitivity: 'base' })
+}
