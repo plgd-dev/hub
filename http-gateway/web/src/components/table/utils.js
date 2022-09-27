6a930bbf27b3e1d6613e217b31e12dcf22b4c287
PatrikMatiasko <patrik.matiasko@gmail.com>
2022-09-27 15:12:20 +0200

    sort improvement

--- http-gateway/web/src/components/table/utils.js
+++ http-gateway/web/src/components/table/utils.js
@@ -3,17 +3,17 @@ export const compareIgnoreCase = (a, b) => {
   let item1 = a
   let item2 = b
 
-  const collator = new Intl.Collator('en', {
+  const collator = new Intl.Collator("en", {
     numeric: true,
-    sensitivity: 'base'
+    sensitivity: "base"
   });
 
-  if(Array.isArray(a)){
-    item1.sort((a, b) => collator.compare(a, b)).join('');
+  if (Array.isArray(a)) {
+    item1 = item1.sort((a, b) => collator.compare(a, b)).join("|")
   }
 
-  if(Array.isArray(b)){
-    item2.sort((a, b) => collator.compare(a, b)).join('');
+  if (Array.isArray(b)) {
+    item2 = item2.sort((a, b) => collator.compare(a, b)).join("|")
   }
 
   return collator.compare(item1, item2)
