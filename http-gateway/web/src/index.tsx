c08a6f06f9984dbbfb34eace40f4a0e93c2bcbd4
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-03-21 13:57:11 +0100

    webpack dep update

--- http-gateway/web/src/index.tsx
+++ http-gateway/web/src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { render } from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { App } from '@/containers/App'
