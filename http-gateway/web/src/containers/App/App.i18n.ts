aa39e744ca759e8e87c8ab578e0fb8b7d5b26e80
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-04-08 22:13:43 +0200

    NotFoundPage.tsx to shared

--- http-gateway/web/src/containers/App/App.i18n.ts
+++ http-gateway/web/src/containers/App/App.i18n.ts
@@ -9,4 +9,12 @@ export const messages = defineMessages({
         id: 'app.authError',
         defaultMessage: 'Authorization server error',
     },
+    pageTitle: {
+        id: 'not-found-page.pageTitle',
+        defaultMessage: 'Page not found KKK',
+    },
+    notFoundPageDefaultMessage: {
+        id: 'not-found-page.notFoundPageDefaultMessage',
+        defaultMessage: 'The page you are looking for does not exist.',
+    },
 })
--- http-gateway/web/src/routes.js
+++ http-gateway/web/src/routes.js
@@ -3,7 +3,9 @@ import DevicesListPage from '@/containers/Devices/List/DevicesListPage'
 import DevicesDetailsPage from '@/containers/Devices/Detail/DevicesDetailsPage'
 import { PendingCommandsListPage } from '@/containers/PendingCommands'
 import Notifications from '@/containers/Notifications'
-import NotFoundPage from '@/containers/NotFoundPage'
+import NotFoundPage from '@shared-ui/components/templates/NotFoundPage'
+import { useIntl } from 'react-intl'
+import { messages as t } from './containers/App/App.i18n'
 
 export const menu = [
     {
@@ -19,16 +21,19 @@ export const menu = [
     },
 ]
 
-export const Routes = () => (
-    <Switch>
-        <Route exact component={DevicesListPage} path='/' />
-        <Route component={DevicesDetailsPage} path={['/devices/:id', '/devices/:id/:href*']} />
-        <Route component={PendingCommandsListPage} path='/pending-commands' />
-        <Route path='/notifications'>
-            <Notifications />
-        </Route>
-        <Route path='*'>
-            <NotFoundPage />
-        </Route>
-    </Switch>
-)
+export const Routes = () => {
+    const { formatMessage: _ } = useIntl()
+    return (
+        <Switch>
+            <Route exact component={DevicesListPage} path='/' />
+            <Route component={DevicesDetailsPage} path={['/devices/:id', '/devices/:id/:href*']} />
+            <Route component={PendingCommandsListPage} path='/pending-commands' />
+            <Route path='/notifications'>
+                <Notifications />
+            </Route>
+            <Route path='*'>
+                <NotFoundPage message={_(t.notFoundPageDefaultMessage)} title={_(t.pageTitle)} />
+            </Route>
+        </Switch>
+    )
+}
