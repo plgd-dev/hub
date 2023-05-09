33fa1bd4a6ea48a45c7ab43d2748784d7efb9e82
Jozef Kralik <jojo.lwin@gmail.com>
2023-05-09 07:46:59 +0000

    test: add new resource in one place

--- cloud2cloud-gateway/service/retrieveResource_test.go
+++ cloud2cloud-gateway/service/retrieveResource_test.go
@@ -163,7 +163,8 @@ func TestRequestHandlerRetrieveResource(t *testing.T) {
 				_ = resp.Body.Close()
 			}()
 			require.Equal(t, tt.wantContentType, resp.Header.Get("Content-Type"))
-			got := testHttp.ReadHTTPResponse(t, resp.Body, tt.wantContentType)
+			var got interface{}
+			testHttp.ReadHTTPResponse(t, resp.Body, tt.wantContentType, &got)
 			if tt.wantContentType == textPlain {
 				require.Contains(t, got, tt.want)
 				return
