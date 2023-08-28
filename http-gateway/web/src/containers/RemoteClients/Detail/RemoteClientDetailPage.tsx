e67040c3f6f85da6e6f6b02ac695499a8ff6a595
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-08-28 23:01:39 +0200

    do not call devices list while remote client is not initialized

--- http-gateway/web/src/containers/RemoteClients/Detail/RemoteClientDetailPage.tsx
+++ http-gateway/web/src/containers/RemoteClients/Detail/RemoteClientDetailPage.tsx
@@ -3,31 +3,39 @@ import { useIntl } from 'react-intl'
 
 import DevicesListPage from '@shared-ui/app/clientApp/Devices/List/DevicesListPage'
 import { messages as menuT } from '@shared-ui/components/Atomic/Menu/Menu.i18n'
+import FullPageLoader from '@shared-ui/components/Atomic/FullPageLoader'
 
 import { messages as t } from '@/containers/RemoteClients/RemoteClients.i18n'
 import RemoteClientsPage from '@/containers/RemoteClients/RemoteClientsPage/RemoteClientsPage'
+import { messages as g } from '@/containers/Global.i18n'
 
 const RemoteClientDetailPage: FC<any> = () => {
     const { formatMessage: _ } = useIntl()
 
     return (
         <RemoteClientsPage>
-            {(clientData) => (
-                <DevicesListPage
-                    breadcrumbs={[
-                        {
-                            link: '/remote-clients',
-                            label: _(t.remoteClients),
-                        },
-                        {
-                            link: `/remote-clients/${clientData.id}`,
-                            label: clientData.clientName,
-                        },
-                    ]}
-                    detailLinkPrefix={`/remote-clients/${clientData?.id}`}
-                    title={`${_(t.remoteClients)} | ${clientData.clientName} | ${_(menuT.devices)}`}
-                />
-            )}
+            {(clientData, wellKnownConfig) => {
+                if (!wellKnownConfig || !wellKnownConfig?.isInitialized) {
+                    return <FullPageLoader i18n={{ loading: _(g.loading) }} />
+                }
+
+                return (
+                    <DevicesListPage
+                        breadcrumbs={[
+                            {
+                                link: '/remote-clients',
+                                label: _(t.remoteClients),
+                            },
+                            {
+                                link: `/remote-clients/${clientData.id}`,
+                                label: clientData.clientName,
+                            },
+                        ]}
+                        detailLinkPrefix={`/remote-clients/${clientData?.id}`}
+                        title={`${_(t.remoteClients)} | ${clientData.clientName} | ${_(menuT.devices)}`}
+                    />
+                )
+            }}
         </RemoteClientsPage>
     )
 }
