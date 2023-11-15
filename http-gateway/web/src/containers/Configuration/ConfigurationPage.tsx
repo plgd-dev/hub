fcb7bbef55cacfc65a31667c6d30086e083f0ada
Patrik Matiaško <patrik.matiasko@gmail.com>
2023-11-15 21:58:33 +0100

    User generated theme from public folder

--- http-gateway/web/src/containers/Configuration/ConfigurationPage.tsx
+++ http-gateway/web/src/containers/Configuration/ConfigurationPage.tsx
@@ -75,7 +75,6 @@ const ConfigurationPage = () => {
     ]
 
     const onSubmit: SubmitHandler<Inputs> = (data) => {
-        console.log(data.theme.value)
         dispatch(setTheme(data.theme.value))
 
         Notification.success(
