bce7c24195eef8be3a190d9c294fd7b1d8af6be8
Daniel Adam <daniel.adam1922@protonmail.com>
2024-06-03 10:35:24 +0200

    Upgrade dependencies
    Submodule:
    github.com/googleapis/googleapis f65ad5f87d253c02e1d854aa48c096acc404215f
    
    Direct:
    github.com/go-co-op/gocron/v2 v2.5.0
    github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0
    github.com/nats-io/nats.go v1.35.0
    github.com/pion/dtls/v2 v2.2.8-0.20240601040544-edc7ad02205f
    github.com/plgd-dev/device/v2 v2.5.2-0.20240603131607-6c9b1dc14b00
    github.com/plgd-dev/go-coap/v3 v3.3.5-0.20240603125124-ff2c47df37c4
    go.opentelemetry.io/contrib/instrumentation/go.mongodb.org/mongo-driver/mongo/otelmongo v0.52.0
    go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.52.0
    go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.52.0
    go.opentelemetry.io/otel v1.27.0
    go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0
    go.opentelemetry.io/otel/metric v1.27.0
    go.opentelemetry.io/otel/sdk v1.27.0
    go.opentelemetry.io/otel/trace v1.27.0
    golang.org/x/exp v0.0.0-20240531132922-fd00a4e0eefc
    golang.org/x/net v0.25.0
    golang.org/x/oauth2 v0.20.0
    google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157
    google.golang.org/grpc v1.64.0
    google.golang.org/protobuf v1.34.1
    
    Indirect:
    cloud.google.com/go/compute/metadata v0.3.0
    github.com/go-json-experiment/json v0.0.0-20240524174822-2d9f40f7385b
    github.com/go-logr/logr v1.4.2
    github.com/goccy/go-json v0.10.3
    github.com/youmark/pkcs8 v0.0.0-20240424034433-3c2c7870ae76
    go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0
    golang.org/x/crypto v0.23.0
    golang.org/x/sys v0.20.0
    golang.org/x/text v0.15.0
    google.golang.org/genproto/googleapis/api v0.0.0-20240528184218-531527333157

--- pkg/net/http/header.go
+++ pkg/net/http/header.go
@@ -4,6 +4,7 @@ const (
 	ApplicationProtoJsonContentType = "application/protojson"
 
 	CorrelationIDHeaderKey      = "Correlation-Id"
+	ContentLengthHeaderKey      = "Content-Length"
 	ContentTypeHeaderKey        = "Content-Type"
 	ContentTypeOptionsHeaderKey = "X-Content-Type-Options"
 	AcceptHeaderKey             = "Accept"
