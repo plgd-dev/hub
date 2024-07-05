ffd1b811f70bd2315139b5f404cbe8e3fbd48be5
Jozef Kralik <jozef.kralik@plgd.dev>
2024-07-05 18:26:13 +0000

    m2moauthserver: fix security vulnerability

--- m2m-oauth-server/uri/uri.go
+++ m2m-oauth-server/uri/uri.go
@@ -4,18 +4,16 @@ import "github.com/lestrrat-go/jwx/v2/jwt"
 
 const (
 	ClientIDKey            = "client_id"
+	ClientSecretKey        = "client_secret"
 	ScopeKey               = "scope"
 	GrantTypeKey           = "grant_type"
-	UsernameKey            = "username"
-	PasswordKey            = "password"
 	AudienceKey            = jwt.AudienceKey
-	SubjectKey             = jwt.SubjectKey
 	AccessTokenKey         = "access_token"
-	DeviceIDKey            = "https://plgd.dev/deviceId"
-	OwnerKey               = "https://plgd.dev/owner"
 	ClientAssertionTypeKey = "client_assertion_type"
 	ClientAssertionTypeJWT = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
 	ClientAssertionKey     = "client_assertion"
+
+	OriginalTokenClaims = "https://plgd.dev/originalClaims"
 
 	Base                = "/m2m-oauth-server"
 	Token               = Base + "/oauth/token"
