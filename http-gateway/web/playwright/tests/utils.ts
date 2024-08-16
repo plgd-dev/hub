1c143110cec2207a8d1c45941b59ae3357e79d2c
Jozef Kralik <jozef.kralik@plgd.dev>
2024-08-16 15:03:40 +0000

    fix regex

--- http-gateway/web/playwright/tests/utils.ts
+++ http-gateway/web/playwright/tests/utils.ts
@@ -4,7 +4,7 @@ export const login = async (page: Page) => {
     await page.goto('http://localhost:3000/')
 
     // keycloak
-    await expect(page).toHaveTitle(/Login | plgd.dev/, { timeout: 30000 })
+    await expect(page).toHaveTitle(/Login|plgd\.dev/, { timeout: 30000 })
     await expect(page).toHaveURL(/auth.plgd.cloud/)
 
     await page.locator('#username').fill(process.env.REACT_APP_TEST_LOGIN_USERNAME || '')
