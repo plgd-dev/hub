237782567c05f280ce6a60b81cb8e4cde493206f
Jozef Kralik <jozef.kralik@plgd.dev>
2025-03-03 21:30:15 +0000

    certificate-authority: support also deprecated API path

--- certificate-authority/service/uri/uri.go
+++ certificate-authority/service/uri/uri.go
@@ -3,9 +3,10 @@ package uri
 import "strings"
 
 const (
-	Base string = "/certificate-authority"
-	API  string = Base + "/api/v1"
-	Sign string = API + "/sign"
+	Base          string = "/certificate-authority"
+	API           string = Base + "/api/v1"
+	DeprecatedAPI string = "/api/v1"
+	Sign          string = API + "/sign"
 
 	SignIdentityCertificate string = Sign + "/identity-csr"
 	SignCertificate         string = Sign + "/csr"
